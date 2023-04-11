c7354d300b6ad7b1d91186ab0db29637743a67f7
Mauricio Araujo <mauricioas@weg.net>
2023-04-11 12:28:06 -0300

    poke-list e poke-search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 //Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
+import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 @NgModule({
-  declarations: [PokeHeaderComponent],
+  declarations: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
   imports: [CommonModule],
-  exports: [PokeHeaderComponent],
+  exports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent],
 })
 export class SharedModule {}
