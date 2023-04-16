f9c43d7d78b4e533c8653d23cac4ad9b502a488b
mauricioaraujo <mauricioaraujo10.ma@gmail.com>
2023-04-16 20:34:09 -0300

    search poke-list

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -7,14 +7,21 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
+  private setAllPokemons: any;
   public getAllPokemons: any;
 
   constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe((res) => {
+      this.setAllPokemons = res.results;
       this.getAllPokemons = res.results;
 
     });
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter((res: any) => { return !res.name.indexOf(value.toLowerCase()) })
+    this.getAllPokemons = filter;
   }
 }
